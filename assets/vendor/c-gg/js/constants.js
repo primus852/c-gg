fb0d4a7a8edd079ab1d261128260106a463bdf22
primus852 <findnibbler@gmail.com>
2016-05-19 00:17:46 +0200

    Inital Commit

---
+++ assets/vendor/c-gg/js/constants.js
@@ -0,0 +1,5 @@
+/* Champion.gg Endpoint to gather all champions */
+const CHAMPION_GG_ENDPOINT_CHAMPIONS = "http://api.champion.gg/";
+
+/* Champion.gg Endpoint to gather a single champion */
+const CHAMPION_GG_ENDPOINT_CHAMPION = "http://api.champion.gg/champion/";
